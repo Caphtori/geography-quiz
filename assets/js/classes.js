32621f9f4ced73f5faf69234c012209fe6f4f534
Qohelet <benjy.shuter@gmail.com>
2023-08-15 18:18:59 -0400

    Adds basic html elements to page.

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -1,13 +1,12 @@
 class country {
     constructor(name, capital, gdp, hos, currency, natAnth, group){
         this.name = name;
-        this.capital = [capital, i];
+        this.capital = capital;
         this.gdp = gdp;
-        this.hos = [hos, i];
-        this.currency = [currency, i];
-        this.natAnth = [natAnth, i]
+        this.hos = hos;
+        this.currency = currency;
+        this.natAnth = natAnth;
         this.group = group;
-        // this.dl = dl
         this.active = [capital, gdp, hos, currency, natAnth];
     }
 };
